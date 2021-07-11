070c23125715726c7864231151f721837bcdafd2
时间轴1854 <hong1854@163.com>
2021-07-11 19:53:00 +0800

    two

---
+++ src/components/content/tabControl/TabControl.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="tab-control">
+    <div v-for="(item, index) in title" :key="item" class="tab-control-item" :class="{active:index ===currentIndex}" @click="itemclick(index)">
+      <span>{{ item }}</span>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      currentIndex:0
+    }
+  },
+  methods: {
+    itemclick(index){
+      this.currentIndex =index
+    }
+  },
+  props: {
+    title: {
+      type: Array,
+      default() {
+        return [];
+      },
+    },
+  },
+};
+</script>
+<style scoped>
+.tab-control {
+  display: flex;
+  text-align: center;
+  font-size: 15px;
+  height: 40px;
+  line-height: 40px;
+
+  position: sticky;
+  top:44px;
+  background-color: #fff;
+}
+.tab-control-item {
+  flex: 1;
+}
+.tab-control-item span {
+  padding: 5px;
+}
+.active{
+  color: var(--color-high-text);
+  
+}
+.active span{
+  border-bottom: 3px solid var(--color-high-text);
+}
+</style>
--- src/views/home/home.vue
+++ src/views/home/home.vue
@@ -2,14 +2,83 @@
   <div id="home">
     <Nav-Bar class="home-navbar"><div slot="center">女人街</div></Nav-Bar>
     <HomeSwiper :banner="banner"></HomeSwiper>
-    <recommend-view :recommends="recommend"></recommend-view>
+    <Recommend-view :recommends="recommend"></Recommend-view>
+    <Feature></Feature>
+    <TabControl :title="['流行','新款','精选']"></TabControl>
+    <ul>
+      <li>1</li>
+      <li>2</li>
+      <li>3</li>
+      <li>4</li>
+      <li>5</li>
+      <li>6</li>
+      <li>7</li>
+      <li>8</li>
+      <li>9</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>2</li>
+      <li>3</li>
+      <li>4</li>
+      <li>5</li>
+      <li>6</li>
+      <li>7</li>
+      <li>8</li>
+      <li>9</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>2</li>
+      <li>3</li>
+      <li>4</li>
+      <li>5</li>
+      <li>6</li>
+      <li>7</li>
+      <li>8</li>
+      <li>9</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+    </ul>
+    
   </div>
+  
 </template>
 
 <script>
 import NavBar from "components/common/navBar/NavBar";
 import HomeSwiper from "./chlidComps/HomeSwiper";
 import RecommendView from "./chlidComps/RecommendView";
+import Feature from "./chlidComps/Feature.vue"
+
+import TabControl from "components/content/tabControl/TabControl"
 
 
 
@@ -26,7 +95,9 @@ export default {
   components: {
     NavBar,
     HomeSwiper,
-    RecommendView
+    RecommendView,
+    Feature,
+    TabControl,
   },
   created() {
     getHomedata().then(res=>{
@@ -39,9 +110,18 @@ export default {
 </script>
 
 <style >
+#home{
+    padding-top:44px;
+  }
 .home-navbar {
   background-color: var(--color-tint);
   color: #fff;
   font-size:13px;
+
+  position: fixed;
+  left:0;
+  right:0;
+  top:0;
+  z-index:9;
 }
 </style>
